3a427cb1e9b1052ce3b517cd2a73260d8c8a2e4b
sosuisen <hidekazu.kubota@gmail.com>
2025-04-29 11:28:42 +0900

    fix: SceneBuilder

--- src/main/java/com/sosuisha/jfxbuilder/SceneBuilder.java
+++ src/main/java/com/sosuisha/jfxbuilder/SceneBuilder.java
@@ -65,55 +65,53 @@ public class SceneBuilder {
                 return in;
             }
     public SceneBuilder apply(java.util.function.Consumer<Scene> func) {
-        if ( in == null ) {
-            build();
-        }
-        func.accept((Scene) in);
+        if (in == null) build();
+func.accept((Scene) in);
         return this;
     }
-    public SceneBuilder camera(Camera value) { in.setCamera(value); return this; }
-    public SceneBuilder cursor(Cursor value) { in.setCursor(value); return this; }
-    public SceneBuilder userAgentStylesheet(String value) { in.setUserAgentStylesheet(value); return this; }
-    public SceneBuilder eventDispatcher(EventDispatcher value) { in.setEventDispatcher(value); return this; }
-    public SceneBuilder onContextMenuRequested(EventHandler<? super ContextMenuEvent> value) { in.setOnContextMenuRequested(value); return this; }
-    public SceneBuilder onMouseClicked(EventHandler<? super MouseEvent> value) { in.setOnMouseClicked(value); return this; }
-    public SceneBuilder onMouseDragged(EventHandler<? super MouseEvent> value) { in.setOnMouseDragged(value); return this; }
-    public SceneBuilder onMouseEntered(EventHandler<? super MouseEvent> value) { in.setOnMouseEntered(value); return this; }
-    public SceneBuilder onMouseExited(EventHandler<? super MouseEvent> value) { in.setOnMouseExited(value); return this; }
-    public SceneBuilder onMouseMoved(EventHandler<? super MouseEvent> value) { in.setOnMouseMoved(value); return this; }
-    public SceneBuilder onMousePressed(EventHandler<? super MouseEvent> value) { in.setOnMousePressed(value); return this; }
-    public SceneBuilder onMouseReleased(EventHandler<? super MouseEvent> value) { in.setOnMouseReleased(value); return this; }
-    public SceneBuilder onDragDetected(EventHandler<? super MouseEvent> value) { in.setOnDragDetected(value); return this; }
-    public SceneBuilder onMouseDragOver(EventHandler<? super MouseDragEvent> value) { in.setOnMouseDragOver(value); return this; }
-    public SceneBuilder onMouseDragReleased(EventHandler<? super MouseDragEvent> value) { in.setOnMouseDragReleased(value); return this; }
-    public SceneBuilder onMouseDragEntered(EventHandler<? super MouseDragEvent> value) { in.setOnMouseDragEntered(value); return this; }
-    public SceneBuilder onMouseDragExited(EventHandler<? super MouseDragEvent> value) { in.setOnMouseDragExited(value); return this; }
-    public SceneBuilder onScrollStarted(EventHandler<? super ScrollEvent> value) { in.setOnScrollStarted(value); return this; }
-    public SceneBuilder onScroll(EventHandler<? super ScrollEvent> value) { in.setOnScroll(value); return this; }
-    public SceneBuilder onScrollFinished(EventHandler<? super ScrollEvent> value) { in.setOnScrollFinished(value); return this; }
-    public SceneBuilder onRotationStarted(EventHandler<? super RotateEvent> value) { in.setOnRotationStarted(value); return this; }
-    public SceneBuilder onRotate(EventHandler<? super RotateEvent> value) { in.setOnRotate(value); return this; }
-    public SceneBuilder onRotationFinished(EventHandler<? super RotateEvent> value) { in.setOnRotationFinished(value); return this; }
-    public SceneBuilder onZoomStarted(EventHandler<? super ZoomEvent> value) { in.setOnZoomStarted(value); return this; }
-    public SceneBuilder onZoom(EventHandler<? super ZoomEvent> value) { in.setOnZoom(value); return this; }
-    public SceneBuilder onZoomFinished(EventHandler<? super ZoomEvent> value) { in.setOnZoomFinished(value); return this; }
-    public SceneBuilder onSwipeUp(EventHandler<? super SwipeEvent> value) { in.setOnSwipeUp(value); return this; }
-    public SceneBuilder onSwipeDown(EventHandler<? super SwipeEvent> value) { in.setOnSwipeDown(value); return this; }
-    public SceneBuilder onSwipeLeft(EventHandler<? super SwipeEvent> value) { in.setOnSwipeLeft(value); return this; }
-    public SceneBuilder onSwipeRight(EventHandler<? super SwipeEvent> value) { in.setOnSwipeRight(value); return this; }
-    public SceneBuilder onTouchPressed(EventHandler<? super TouchEvent> value) { in.setOnTouchPressed(value); return this; }
-    public SceneBuilder onTouchMoved(EventHandler<? super TouchEvent> value) { in.setOnTouchMoved(value); return this; }
-    public SceneBuilder onTouchReleased(EventHandler<? super TouchEvent> value) { in.setOnTouchReleased(value); return this; }
-    public SceneBuilder onTouchStationary(EventHandler<? super TouchEvent> value) { in.setOnTouchStationary(value); return this; }
-    public SceneBuilder onDragEntered(EventHandler<? super DragEvent> value) { in.setOnDragEntered(value); return this; }
-    public SceneBuilder onDragExited(EventHandler<? super DragEvent> value) { in.setOnDragExited(value); return this; }
-    public SceneBuilder onDragOver(EventHandler<? super DragEvent> value) { in.setOnDragOver(value); return this; }
-    public SceneBuilder onDragDropped(EventHandler<? super DragEvent> value) { in.setOnDragDropped(value); return this; }
-    public SceneBuilder onDragDone(EventHandler<? super DragEvent> value) { in.setOnDragDone(value); return this; }
-    public SceneBuilder onKeyPressed(EventHandler<? super KeyEvent> value) { in.setOnKeyPressed(value); return this; }
-    public SceneBuilder onKeyReleased(EventHandler<? super KeyEvent> value) { in.setOnKeyReleased(value); return this; }
-    public SceneBuilder onKeyTyped(EventHandler<? super KeyEvent> value) { in.setOnKeyTyped(value); return this; }
-    public SceneBuilder onInputMethodTextChanged(EventHandler<? super InputMethodEvent> value) { in.setOnInputMethodTextChanged(value); return this; }
-    public SceneBuilder userData(Object value) { in.setUserData(value); return this; }
-    public SceneBuilder nodeOrientation(NodeOrientation value) { in.setNodeOrientation(value); return this; }
+    public SceneBuilder camera(Camera value) { if (in == null) build(); in.setCamera(value); return this; }
+    public SceneBuilder cursor(Cursor value) { if (in == null) build(); in.setCursor(value); return this; }
+    public SceneBuilder userAgentStylesheet(String value) { if (in == null) build(); in.setUserAgentStylesheet(value); return this; }
+    public SceneBuilder eventDispatcher(EventDispatcher value) { if (in == null) build(); in.setEventDispatcher(value); return this; }
+    public SceneBuilder onContextMenuRequested(EventHandler<? super ContextMenuEvent> value) { if (in == null) build(); in.setOnContextMenuRequested(value); return this; }
+    public SceneBuilder onMouseClicked(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMouseClicked(value); return this; }
+    public SceneBuilder onMouseDragged(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMouseDragged(value); return this; }
+    public SceneBuilder onMouseEntered(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMouseEntered(value); return this; }
+    public SceneBuilder onMouseExited(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMouseExited(value); return this; }
+    public SceneBuilder onMouseMoved(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMouseMoved(value); return this; }
+    public SceneBuilder onMousePressed(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMousePressed(value); return this; }
+    public SceneBuilder onMouseReleased(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnMouseReleased(value); return this; }
+    public SceneBuilder onDragDetected(EventHandler<? super MouseEvent> value) { if (in == null) build(); in.setOnDragDetected(value); return this; }
+    public SceneBuilder onMouseDragOver(EventHandler<? super MouseDragEvent> value) { if (in == null) build(); in.setOnMouseDragOver(value); return this; }
+    public SceneBuilder onMouseDragReleased(EventHandler<? super MouseDragEvent> value) { if (in == null) build(); in.setOnMouseDragReleased(value); return this; }
+    public SceneBuilder onMouseDragEntered(EventHandler<? super MouseDragEvent> value) { if (in == null) build(); in.setOnMouseDragEntered(value); return this; }
+    public SceneBuilder onMouseDragExited(EventHandler<? super MouseDragEvent> value) { if (in == null) build(); in.setOnMouseDragExited(value); return this; }
+    public SceneBuilder onScrollStarted(EventHandler<? super ScrollEvent> value) { if (in == null) build(); in.setOnScrollStarted(value); return this; }
+    public SceneBuilder onScroll(EventHandler<? super ScrollEvent> value) { if (in == null) build(); in.setOnScroll(value); return this; }
+    public SceneBuilder onScrollFinished(EventHandler<? super ScrollEvent> value) { if (in == null) build(); in.setOnScrollFinished(value); return this; }
+    public SceneBuilder onRotationStarted(EventHandler<? super RotateEvent> value) { if (in == null) build(); in.setOnRotationStarted(value); return this; }
+    public SceneBuilder onRotate(EventHandler<? super RotateEvent> value) { if (in == null) build(); in.setOnRotate(value); return this; }
+    public SceneBuilder onRotationFinished(EventHandler<? super RotateEvent> value) { if (in == null) build(); in.setOnRotationFinished(value); return this; }
+    public SceneBuilder onZoomStarted(EventHandler<? super ZoomEvent> value) { if (in == null) build(); in.setOnZoomStarted(value); return this; }
+    public SceneBuilder onZoom(EventHandler<? super ZoomEvent> value) { if (in == null) build(); in.setOnZoom(value); return this; }
+    public SceneBuilder onZoomFinished(EventHandler<? super ZoomEvent> value) { if (in == null) build(); in.setOnZoomFinished(value); return this; }
+    public SceneBuilder onSwipeUp(EventHandler<? super SwipeEvent> value) { if (in == null) build(); in.setOnSwipeUp(value); return this; }
+    public SceneBuilder onSwipeDown(EventHandler<? super SwipeEvent> value) { if (in == null) build(); in.setOnSwipeDown(value); return this; }
+    public SceneBuilder onSwipeLeft(EventHandler<? super SwipeEvent> value) { if (in == null) build(); in.setOnSwipeLeft(value); return this; }
+    public SceneBuilder onSwipeRight(EventHandler<? super SwipeEvent> value) { if (in == null) build(); in.setOnSwipeRight(value); return this; }
+    public SceneBuilder onTouchPressed(EventHandler<? super TouchEvent> value) { if (in == null) build(); in.setOnTouchPressed(value); return this; }
+    public SceneBuilder onTouchMoved(EventHandler<? super TouchEvent> value) { if (in == null) build(); in.setOnTouchMoved(value); return this; }
+    public SceneBuilder onTouchReleased(EventHandler<? super TouchEvent> value) { if (in == null) build(); in.setOnTouchReleased(value); return this; }
+    public SceneBuilder onTouchStationary(EventHandler<? super TouchEvent> value) { if (in == null) build(); in.setOnTouchStationary(value); return this; }
+    public SceneBuilder onDragEntered(EventHandler<? super DragEvent> value) { if (in == null) build(); in.setOnDragEntered(value); return this; }
+    public SceneBuilder onDragExited(EventHandler<? super DragEvent> value) { if (in == null) build(); in.setOnDragExited(value); return this; }
+    public SceneBuilder onDragOver(EventHandler<? super DragEvent> value) { if (in == null) build(); in.setOnDragOver(value); return this; }
+    public SceneBuilder onDragDropped(EventHandler<? super DragEvent> value) { if (in == null) build(); in.setOnDragDropped(value); return this; }
+    public SceneBuilder onDragDone(EventHandler<? super DragEvent> value) { if (in == null) build(); in.setOnDragDone(value); return this; }
+    public SceneBuilder onKeyPressed(EventHandler<? super KeyEvent> value) { if (in == null) build(); in.setOnKeyPressed(value); return this; }
+    public SceneBuilder onKeyReleased(EventHandler<? super KeyEvent> value) { if (in == null) build(); in.setOnKeyReleased(value); return this; }
+    public SceneBuilder onKeyTyped(EventHandler<? super KeyEvent> value) { if (in == null) build(); in.setOnKeyTyped(value); return this; }
+    public SceneBuilder onInputMethodTextChanged(EventHandler<? super InputMethodEvent> value) { if (in == null) build(); in.setOnInputMethodTextChanged(value); return this; }
+    public SceneBuilder userData(Object value) { if (in == null) build(); in.setUserData(value); return this; }
+    public SceneBuilder nodeOrientation(NodeOrientation value) { if (in == null) build(); in.setNodeOrientation(value); return this; }
 }
