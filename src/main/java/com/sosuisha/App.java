705ce9b332384c53851814142bb3136185986cd2
sosuisen <hidekazu.kubota@gmail.com>
2025-04-29 21:30:55 +0900

    feat: use jdk24

--- src/main/java/com/sosuisha/App.java
+++ src/main/java/com/sosuisha/App.java
@@ -9,18 +9,16 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        // コマンドライン引数を取得
         Parameters params = getParameters();
         String filePath = params.getUnnamed().isEmpty() ? null : params.getUnnamed().get(0);
         if (filePath != null) {
             File file = new File(filePath);
             if (file.exists() && file.isFile()) {
-                new ImageViewerWindow(file, true); // 指定された画像ファイルを開く
+                new ImageViewerWindow(file, true);
             } else {
-                System.err.println("指定されたファイルが存在しないか、無効です: " + filePath);
+                System.err.println("File not found: " + filePath);
             }
         } else {
-            // 引数がない場合はドラッグ＆ドロップ可能なウィンドウを開く
             new DragAndDropWindow(stage);
         }
     }
