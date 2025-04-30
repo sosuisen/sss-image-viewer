05bbe274f68eea77a3217827f700f37f14f7aeed
sosuisen <hidekazu.kubota@gmail.com>
2025-04-30 20:13:51 +0900

    feat: native build using GluonGraalJDK23

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.sosuisha.imageviewer {
-    requires javafx.controls;
-    requires javafx.swing;
-    requires transitive javafx.graphics;
-    exports com.sosuisha.imageviewer to javafx.graphics, javafx.controls;
-}
