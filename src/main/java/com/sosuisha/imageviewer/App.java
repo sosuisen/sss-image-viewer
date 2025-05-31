72746bc472fbfd3448d1745e4fcd410f17e85dc9
sosuisen <hidekazu.kubota@gmail.com>
2025-05-31 22:24:32 +0900

    feat: add startup error dialog

--- src/main/java/com/sosuisha/imageviewer/App.java
+++ src/main/java/com/sosuisha/imageviewer/App.java
@@ -1,6 +1,9 @@
 package com.sosuisha.imageviewer;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -17,6 +20,8 @@ public class App extends Application {
                 new ImageViewerWindow(file, true);
             } else {
                 System.err.println("File not found: " + filePath);
+                showStartupErrorAndExit("Startup Error: ", new IllegalArgumentException("File not found: " + filePath), false);
+                new DragAndDropWindow(stage);
             }
         } else {
             new DragAndDropWindow(stage);
@@ -25,5 +30,24 @@ public class App extends Application {
 
     public static void main(String[] args) {
         Application.launch(App.class, args);
+    }
+
+    private void showStartupErrorAndExit(String message, Exception e, boolean canExit) {
+        e.printStackTrace();
+
+        Platform.runLater(() -> {
+            var alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Startup Error");
+            alert.setHeaderText("An error occurred during startup");
+            alert.getDialogPane().setPrefWidth(480);
+            alert.getDialogPane().setPrefHeight(320);
+            
+            alert.getDialogPane().setExpandableContent(new Label(message + ": " + e.getMessage()));
+            alert.getDialogPane().setExpanded(true);
+            if(canExit) {
+                alert.setOnHidden(ev -> Platform.exit());
+            }
+            alert.show();
+        });
     }
 }
