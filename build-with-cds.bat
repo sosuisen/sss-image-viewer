01f6b65c419b3f1bfb60b25a3eec219cdd45f060
sosuisen <hidekazu.kubota@gmail.com>
2025-07-07 02:12:02 +0900

    fix: open image

--- build-with-cds.bat
+++ build-with-cds.bat
@@ -4,16 +4,14 @@ cd target/package
 
 ..\image\bin\java -Xshare:dump
 
-..\image\bin\java -XX:ArchiveClassesAtExit=app.jsa -Dcds.training.mode=true -jar sss-image-viewer-1.0.5.jar ../../images/sample.png
+echo Open image
+..\image\bin\java -XX:ArchiveClassesAtExit=app.jsa -Dcds.training.mode=true -jar sss-image-viewer-1.0.5.jar "..\..\images\sample.png"
 
 if exist app.jsa (
     echo.
     echo Dynamic CDS archive created successfully!
     dir app.jsa
     
-    echo.
-    echo Testing with Dynamic CDS...
-    ..\image\bin\java -XX:SharedArchiveFile=app.jsa -jar sss-image-viewer-1.0.5.jar
 ) else (
     echo Failed to create Dynamic CDS archive
 )
